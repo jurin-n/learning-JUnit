85e75929158114f2c7be13a4fb56b73df0967d1b
jurin-n <jurin.nakano@gmail.com>
2015-09-05 20:06:28 +0900

    modify code for use Apache Http Client Fluent API.

--- src/main/java/com/jurin_n/junit/restapi/test/BookResourceTest.java
+++ src/main/java/com/jurin_n/junit/restapi/test/BookResourceTest.java
@@ -1,58 +1,65 @@
 package com.jurin_n.junit.restapi.test;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import static org.hamcrest.core.Is.is;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
 
 public class BookResourceTest extends RestApiTest {
-
-
-	@Test
-	public void test() {
-		/* セットアップ */
-		//Httpヘッダ
-		headers.put("Content-Type","application/json");
-		headers.put("Accept","application/json");
-		headers.put("Date","xxxx");
-		headers.put("Authorization","xxxx");
+	@Before
+	public void setUp(){
 		
-		//リクエスト送信、レスポンス取得
-		response = request.setResource("/hoge")
-						  .setMethod("GET")
-						  .setHeaders(headers)
-						  .setBody(getRequestBody("/requestBody.json"))
-						  .send();
-		
-		/* 検証 */
-		assertThat(response.getHeader("Content-Type"),is("application/json"));
-		assertThat(response.getStatusCode(),is(200));
-		assertThat(response.getBody(),is(expectedBody("/expectedBody.json")));
 	}
-
 	
+	@After
+	public void tearDown(){
+        if (response != null) {
+            response.discardContent();
+        }
+	}
+	
+	//Apache Http Client Fluent APIつかった場合 
 	@Test
-	public void test2() {
-		/* セットアップ */
-		//リソース URI
-		request.setResource("/hoge");
-		//Httpメソッド
-		request.setMethod("GET");
-		//Httpヘッダ
-		headers.put("Content-Type","application/json");
-		headers.put("Accept","application/json");
-		headers.put("Date","xxxx");
-		headers.put("Authorization","xxxx");
-		request.setHeaders(headers);
-		//リクエストボディ
-		request.setBody(getRequestBody("/requestBody.json"));
-		
+	public void test() throws IOException {
+	   	/* セットアップ */
+	   	// headers
+	   	int i = 4; //何かの仕組みで配列インデックスを決める。
+	   	Header[] headers = new Header[i];
+    	headers[0]= new BasicHeader("Content-Type","application/json");
+    	headers[1]= new BasicHeader("Accept","application/json");
+    	headers[2]= new BasicHeader("Date","xxxx");
+    	headers[3]= new BasicHeader("Authorization","xxxx");
+	    	
+    	//body
+		HttpEntity entity = new StringEntity("body!",StandardCharsets.UTF_8);
+
 		/* リクエスト送信、レスポンス取得 */
-		response = request.send();
-		
+		response = Request
+			        .Get("http://www.yahoo.co.jp/")
+			        .setHeaders(headers)
+			        //.body(entity) //Getの場合、IllegalStateException
+			        .execute();
 		/* 検証 */
-		assertThat(response.getHeader("Content-Type"),is("application/json"));
-		assertThat(response.getStatusCode(),is(200));
-		assertThat(response.getBody(),is(expectedBody("/expectedBody.json")));
+		assertThat(
+				 response.returnResponse().getHeaders("Content-Type")
+				,is("application/json")
+				);
+		assertThat(
+				 response.returnResponse().getStatusLine().getStatusCode()
+				,is(200));
+		assertThat(
+				 response.returnContent().asString(StandardCharsets.UTF_8)
+				,is(expectedBody("/expectedBody.json")));	
 	}
 }
--- src/main/java/com/jurin_n/junit/restapi/test/RestApiTest.java
+++ src/main/java/com/jurin_n/junit/restapi/test/RestApiTest.java
@@ -1,11 +1,10 @@
 package com.jurin_n.junit.restapi.test;
 
-import java.util.HashMap;
+import org.apache.http.client.fluent.Response;
 
 public class RestApiTest {
-	protected HashMap<String,String> headers;
-	protected HttpRequest request;
-	protected HttpResponse response;
+	Response response = null;
+	
 	protected String getRequestBody(String source){
 		return "body";
 	}
