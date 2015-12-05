3c6708f1323320acc68d30a5abe464f40ad7818f
jurin-n <jurin.nakano@gmail.com>
2015-12-05 23:51:45 +0900

    リファクタリング練習用コード追加（リファクタリング前）。

---
+++ src/main/java/com/jurin_n/refactoring/Customer.java
@@ -0,0 +1,69 @@
+package com.jurin_n.refactoring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+	private String name;
+	private List<Rental> rentals = new ArrayList<>();
+	
+	public Customer(String name){
+		this.name = name;
+	}
+	
+	public void addRental(Rental rental){
+		rentals.add(rental);
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String statement(){
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		String result = "Rental Record for " + getName() + "\n";
+		for(Rental rental : rentals){
+			double thisAmount = 0;
+			
+			//一行ごとに金額を計算
+			switch(rental.getMovie().getPriceCode()){
+				case Movie.REGULAR:
+					thisAmount += 2;
+					if(rental.getDaysRented() >= 2){
+						thisAmount  += (rental.getDaysRented() -2) * 1.5;
+					}
+					break;
+				case Movie.NEW_RELEASE:
+					thisAmount += rental.getDaysRented() * 3;
+					break;
+				case Movie.CHILDRENS:
+					thisAmount += 1.5;
+					if(rental.getDaysRented() > 3){
+						thisAmount += (rental.getDaysRented() - 3) * 1.5;
+					}
+					break;
+			}
+			
+			//レンタルポイントを加算
+			frequentRenterPoints++;
+			
+			//新作を二日以上借りた場合はボーナスポイント
+			if((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+				rental.getDaysRented() > 1){
+				frequentRenterPoints++;
+			}
+			
+			//この貸し出しに関する数値の表示
+			result += "\t" + rental.getMovie().getTitle() + "\t" +
+						thisAmount + "\n";
+			totalAmount += thisAmount;
+		}
+		
+		//フッタ部分の追加
+		result += "Amount owed is " + totalAmount + "\n";
+		result += "You earned " + frequentRenterPoints +
+				" frequent renter points";
+		return result;
+	}
+}
