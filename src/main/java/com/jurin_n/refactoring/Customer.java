f4768917a890903bf2e0ac9125966ff29056de7c
jurin-n <jurin.nakano@gmail.com>
2015-12-06 09:36:27 +0900

    switch文の長い処理をメソッドとし抽出

--- src/main/java/com/jurin_n/refactoring/Customer.java
+++ src/main/java/com/jurin_n/refactoring/Customer.java
@@ -24,26 +24,7 @@ public class Customer {
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		for(Rental rental : rentals){
-			double thisAmount = 0;
-			
-			//一行ごとに金額を計算
-			switch(rental.getMovie().getPriceCode()){
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if(rental.getDaysRented() >= 2){
-						thisAmount  += (rental.getDaysRented() -2) * 1.5;
-					}
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += rental.getDaysRented() * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if(rental.getDaysRented() > 3){
-						thisAmount += (rental.getDaysRented() - 3) * 1.5;
-					}
-					break;
-			}
+			double thisAmount = amountFor(rental);
 			
 			//レンタルポイントを加算
 			frequentRenterPoints++;
@@ -65,5 +46,29 @@ public class Customer {
 		result += "You earned " + frequentRenterPoints +
 				" frequent renter points";
 		return result;
+	}
+
+	private double amountFor(Rental rental) {
+		double thisAmount = 0;
+		
+		//一行ごとに金額を計算
+		switch(rental.getMovie().getPriceCode()){
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if(rental.getDaysRented() >= 2){
+					thisAmount  += (rental.getDaysRented() -2) * 1.5;
+				}
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += rental.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if(rental.getDaysRented() > 3){
+					thisAmount += (rental.getDaysRented() - 3) * 1.5;
+				}
+				break;
+		}
+		return thisAmount;
 	}
 }
