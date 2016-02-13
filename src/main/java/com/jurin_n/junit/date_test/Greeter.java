cf063d83740454e91cf943b34610aac0f71a865c
jurin-n <jurin.nakano@gmail.com>
2016-02-13 23:05:42 +0900

    日付に関するテストを構造化テストとパラメータ化テストで作成してみた。

---
+++ src/main/java/com/jurin_n/junit/date_test/Greeter.java
@@ -0,0 +1,15 @@
+package com.jurin_n.junit.date_test;
+
+import java.util.Calendar;
+
+public class Greeter {
+	public String greet(Calendar now) {
+		int hour = now.get(Calendar.HOUR_OF_DAY);
+		if(hour>=5 && hour<12){
+			return "おはようございます";
+		}else if(hour>=12 && hour<18){
+			return "こんにちは";
+		}
+		return "こんばんは";
+	}
+}
---
+++ src/test/java/com/jurin_n/junit/date_test/GreetTest.java
@@ -0,0 +1,103 @@
+package com.jurin_n.junit.date_test;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
+import org.junit.BeforeClass;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(Enclosed.class)
+public class GreetTest {
+	
+	@RunWith(Theories.class)
+	public static class 朝05時以上_12時未満_の場合{
+
+		@DataPoints
+		public static Calendar CALENDAR[];
+
+		@BeforeClass
+		public static void setUpCalendar() {
+			CALENDAR = new Calendar[]{
+					 newCalendar(Calendar.getInstance(),5,0,0,0)
+					,newCalendar(Calendar.getInstance(),11,59,59,999)
+					};
+		}
+
+		//朝(05:00:00以上 12:00:00未満)の場合、「おはようございます」と返す
+		@Theory
+		public void 朝05時以上_12時未満_の場合_おはようございます_と返す(Calendar cal) {
+			Greeter sut = new Greeter();
+			System.out.println(testDataLoggin(cal));
+			assertThat(cal.toString(), sut.greet(cal), is("おはようございます"));
+		}
+	};
+
+	@RunWith(Theories.class)
+	public static class 昼12時以上_18時未満_の場合{
+		@DataPoints
+		public static Calendar CALENDAR[];
+
+		@BeforeClass
+		public static void setUpCalendar() {
+			CALENDAR = new Calendar[]{
+					 newCalendar(Calendar.getInstance(),12,0,0,0)
+					,newCalendar(Calendar.getInstance(),17,59,59,999)
+					};
+		}
+		
+		//昼(12:00:00以上 18:00:00未満)の場合、「こんにちは」と返す
+		@Theory
+		public void 昼12時以上_18時未満_の場合_こんにちは_と返す(Calendar cal) {
+			Greeter sut = new Greeter();
+			System.out.println(testDataLoggin(cal));
+			assertThat(cal.toString(), sut.greet(cal), is("こんにちは"));
+		}
+	};
+	
+	@RunWith(Theories.class)
+	public static class 夜18時以上_05時未満_の場合{
+		@DataPoints
+		public static Calendar CALENDAR[];
+
+		@BeforeClass
+		public static void setUpCalendar() {
+			CALENDAR = new Calendar[]{
+					 newCalendar(Calendar.getInstance(),18,0,0,0)
+					,newCalendar(Calendar.getInstance(),23,59,59,999)
+					,newCalendar(Calendar.getInstance(),0,0,0,0)
+					,newCalendar(Calendar.getInstance(),4,59,59,999)
+					};
+		}
+		
+		//夜(18:00:00以上 05:00:00未満)の場合、「こんばんは」と返す/
+		@Theory
+		public void 夜18時以上_05時未満_の場合_こんばんは_と返す(Calendar cal) {
+			Greeter sut = new Greeter();
+			System.out.println(testDataLoggin(cal));
+			assertThat(cal.toString(), sut.greet(cal), is("こんばんは"));
+		}
+	};
+	static String testDataLoggin(Calendar cal){
+		return "[testing・・・]"
+				+ " HOUR_OF_DAY=" + cal.get(Calendar.HOUR_OF_DAY)
+				+ ",MINUTE=" + cal.get(Calendar.MINUTE)
+				+ ",SECOND=" + cal.get(Calendar.SECOND)
+				+ ",MILLISECOND=" + cal.get(Calendar.MILLISECOND)
+				;
+	}
+	static Calendar newCalendar(Calendar today, int hour, int minute, int second, int millisecond){
+		Calendar cal = today;
+		cal.set(Calendar.HOUR_OF_DAY, hour);
+		cal.set(Calendar.MINUTE, minute);
+		cal.set(Calendar.SECOND, second);
+		cal.set(Calendar.MILLISECOND, millisecond);
+		return cal;
+	}
+}
